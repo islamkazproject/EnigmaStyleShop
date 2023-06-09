08e8520064b6bbc24942fa7c5b98575aa357b1c8
islam <islamkazproject@gmail.com>
2023-06-09 21:33:32 +0300

    Add media, edit views, url. Delete test_context.html file. Added dynamic content.

--- esshop_project/products/views.py
+++ esshop_project/products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from products.models import Product, ProductCategory
+
 
 def index(request):
     context = {
@@ -10,24 +12,9 @@ def index(request):
 
 def products(request):
     context = {
-        'title': 'ESShop - Каталог'
+        'title': 'ESShop - Каталог',
+        'categories': ProductCategory.objects.all(),
+        'products': Product.objects.all(),
     }
     return render(request, 'products/products.html', context)
 
-
-def test_context(request):
-    context = {
-        'title': 'store',
-        'header': 'Welcome',
-        'username': 'Ivan Ivanov',
-        'products': [
-            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090.00},
-            {'name': 'Синяя куртка The North Face', 'price': 23725.00},
-        ],
-        'promotion': True,
-        'products_of_promotion': [
-            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390.00},
-            {'name': 'Черный рюкзак Nike Heritage', 'price': 2340.00},
-        ]
-    }
-    return render(request, 'products/test_context.html', context)
