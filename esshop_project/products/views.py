0a0d06594cb98b3aff4a3cc72257594a411b1f5e
islam <islamkazproject@gmail.com>
2023-06-09 15:10:29 +0300

    Add base.html file, add code in views and url.

--- esshop_project/products/views.py
+++ esshop_project/products/views.py
@@ -2,8 +2,32 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'ESShop'
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'ESShop - Каталог'
+    }
+    return render(request, 'products/products.html', context)
+
+
+def test_context(request):
+    context = {
+        'title': 'store',
+        'header': 'Welcome',
+        'username': 'Ivan Ivanov',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090.00},
+            {'name': 'Синяя куртка The North Face', 'price': 23725.00},
+        ],
+        'promotion': True,
+        'products_of_promotion': [
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390.00},
+            {'name': 'Черный рюкзак Nike Heritage', 'price': 2340.00},
+        ]
+    }
+    return render(request, 'products/test_context.html', context)
