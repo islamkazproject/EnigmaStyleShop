de2448da56eb6af33a062b4f8c2f06cff029c810
islam <islamkazproject@gmail.com>
2023-06-11 17:52:30 +0300

    Add form and controllers for form UserProfileForm: profile. Now the user has a personal account, and he can change personal information

--- esshop_project/users/views.py
+++ esshop_project/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import auth
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
 def login(request):
@@ -37,3 +37,18 @@ def register(request):
         'form': form
     }
     return render(request, 'users/register.html', context)
+
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(data=request.POST, files=request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {
+        'title': 'Enigma Style Shop -  Профиль',
+        'form': form
+    }
+    return render(request, 'users/profile.html', context)
