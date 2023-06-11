505c15510d15dd9756ae1cddbfee66183e04f9a5
islam <islamkazproject@gmail.com>
2023-06-11 16:50:28 +0300

    Add form and controllers for form UserLoginForm: login, UserRegistrationForm: register. Edit templates form. Add base.html for login.html, register.html

--- esshop_project/users/views.py
+++ esshop_project/users/views.py
@@ -1,9 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import auth
+
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {
+        'title': 'Enigma Style Shop -  Авторизация',
+        'form': form
+    }
+    return render(request, 'users/login.html', context)
 
 
 def register(request):
-    return render(request, 'users/register.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+    context = {
+        'title': 'Enigma Style Shop -  Регистрация',
+        'form': form
+    }
+    return render(request, 'users/register.html', context)
